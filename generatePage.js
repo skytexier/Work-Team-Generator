34f40f3a0ebe7f95a23c349eac14f00ec5841101
Sky Texier <skyhamilton@gmail.com>
2022-10-24 16:28:37 -0700

    Script finalized, adjusting HTML and CSS styling
    Script finalized to append appropriate information from inquirer, still need to adjust page styling and set up tests

--- generatePage.js
+++ generatePage.js
@@ -1,45 +1,69 @@
-const Manager = require("./lib/Manager");
+const generatePage = team => {
+    console.log(team)
+    const getManager = manager => {
+    return `<div class="card">
+    <div class="card-body">
+        <h3 class="card-title"></h3>
+        <h4>${manager.getName()}</h4>
+    </div>
+    </div>`
+    };
+    const getEngineer = engineer => {
+        return `<div class="card">
+        <div class="card-body">
+            <h3 class="card-title"></h3>
+            <h4>${engineer.getName()}</h4>
+        </div>
+        </div>`
+        };
+    const getIntern = intern => {
+        return `<div class="card">
+        <div class="card-body">
+            <h3 class="card-title"></h3>
+            <h4>${intern.getName()}</h4>
+        </div>
+        </div>`
+        };
 
-function generatePage(team){
-return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Builder</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<header>
-    <h1 class="d-flex justify-content-center m-3 bg-secondary">Crew Sheet</h1>
-</header>
-<body>
-<section class="d-flex justify-content-center container">
-    <div class="card">
-        <div class="card-body">
-            <h3 class="card-title">${Manager.getName()}</h3>
-            <h4 class="card-body">${Manager.getEmail()}</h3>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <h3 class="card-title">${team}</h3>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <h3 class="card-title">Intern</h3>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <h3 class="card-title">Manager</h3>
-        </div>
-    </div>
-</section>
-</body>
-</html>
-`
+const html = [];
+
+html.push(team
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => getManager(manager))
+);
+html.push(team
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => getEngineer(engineer))
+    .join("")
+);
+html.push(team
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => getIntern(intern))
+    .join("")
+);
+
+return html.join("");
 }
 
-module.exports = generatePage;
+module.exports = team => {     
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Builder</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <header>
+        <h1 class="d-flex justify-content-center m-3 bg-secondary">Crew Sheet</h1>
+    </header>
+    <body>
+    <section class="d-flex justify-content-center container">
+    ${generatePage(team)}
+    </section>
+    </body>
+    </html>
+    `
+    
+};
--- index.js
+++ index.js
@@ -1,152 +1,152 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const fs = require("fs");
-const generatePage = require('./generatePage');
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
-const team = [];
+const generatePage = require("./generatePage");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+let team = [];
 
 const roleSelect = [
-    {
-        type: "list",
-        message: "What type of employee would to like to add?",
-        choices: ["Manager", "Engineer", "Intern", "None"],
-        name: "choices"
-        
-    }
-]
+  {
+    type: "list",
+    message: "What type of employee would to like to add?",
+    choices: ["Manager", "Engineer", "Intern", "None"],
+    name: "choices",
+  },
+];
 const engineerQuestions = [
-    {
-        type: "input",
-        message: "What is this employees name?",
-        name: "name",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees ID?",
-        name: "id",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees email?",
-        name: "email",
-        
-    },
-    {
-        type: "input",
-        message: "What is this engineers Github",
-        name: "github",
-        
-    }
-]
+  {
+    type: "input",
+    message: "What is this employees name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is this employees ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is this employees email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is this engineers Github",
+    name: "github",
+  },
+];
 
 const internQuestions = [
-    {
-        type: "input",
-        message: "What is this employees name?",
-        name: "name",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees ID?",
-        name: "id",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees email?",
-        name: "email",
-        
-    },
-    {
-        type: "input",
-        message: "What is this interns university/school name?",
-        name: "school",
-        
-    }
-]
+  {
+    type: "input",
+    message: "What is this employees name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is this employees ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is this employees email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is this interns university/school name?",
+    name: "school",
+  },
+];
 
 const managerQuestions = [
-    {
-        type: "input",
-        message: "What is this employees name?",
-        name: "managerName",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees ID?",
-        name: "id",
-        
-    },
-    {
-        type: "input",
-        message: "What is this employees email?",
-        name: "email",
-        
-    },
-    {
-        type: "input",
-        message: "What is this managers office number?",
-        name: "officeNumber",
-        
+  {
+    type: "input",
+    message: "What is this employees name?",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "What is this employees ID?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is this employees email?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is this managers office number?",
+    name: "officeNumber",
+  },
+];
+
+function init() {
+  return inquirer.prompt(roleSelect).then((answer) => {
+    let role = answer.choices;
+    console.log(role + " selected!");
+    switch (role) {
+      case "Engineer":
+        inquirer.prompt(engineerQuestions).then((data) => {
+          const engineer = new Engineer(
+            data.name,
+            data.id,
+            data.email,
+            data.github
+          );
+          team.push(engineer);
+          console.log("Engineer added to roster");
+          init();
+        });
+        break;
+
+      case "Intern":
+        inquirer.prompt(internQuestions).then((data) => {
+          const intern = new Intern(
+            data.name,
+            data.id,
+            data.email,
+            data.school
+          );
+          team.push(intern);
+          console.log("Intern added to roster");
+          init();
+        });
+        break;
+
+      case "Manager":
+        inquirer.prompt(managerQuestions).then((data) => {
+          const manager = new Manager(
+            data.managerName,
+            data.id,
+            data.email,
+            data.officeNumber
+          );
+          team.push(manager);
+          console.log(manager);
+          console.log("Manager added to roster");
+          init();
+        });
+        break;
+      case "None":
+        console.log(JSON.stringify(team));
+        console.log(team);
+        console.log("No more employees will be added, thank you");
+        console.log("Your team roster will now be generated");
+        writeToFile(generatePage(team));
+        break;
+      default:
+        return "Error adding this employee";
     }
-]
-
-function init(data) {
-    return inquirer.prompt(roleSelect).then((answer) => {
-        let role = answer.choices;
-        console.log(role + " selected!")
-        switch (role){
-                case "Engineer":
-                    inquirer.prompt(engineerQuestions).then((data) => {    
-                        const engineer = new Engineer(data.name, data.id, data.email, data.github)
-                        team.push(engineer)
-                        console.log("Engineer added to roster")
-                        init()
-                        })
-                    break;
-                
-                case "Intern":
-                    inquirer.prompt(internQuestions).then((data) => {
-                        const intern = new Intern(data.name, data.id, data.email, data.school)
-                        team.push(intern)
-                        console.log("Intern added to roster");
-                        init()
-                    })
-                    break;
-    
-                case "Manager":
-                    inquirer.prompt(managerQuestions).then((data) => {
-                        const manager = new Manager(data.managerName, data.id, data.email, data.officeNumber)
-                        team.push(manager)
-                        console.log("Manager added to roster");
-                        init()
-                    })
-                    break;
-                case "None":
-                    console.log(JSON.stringify(team));
-                    let crew = JSON.stringify(team)
-                    console.log("No more employees will be added, thank you")
-                    console.log("Your team roster will now be generated")
-                    writeToFile(generatePage(team));
-                    break;
-                default:
-                    return "Error adding this employee";
-        }
-    }) 
-};
-
-function writeToFile (filename, data){
-    fs.writeFile("index.html", filename, (err, data) => {
-        err ? console.log(err) : console.log("Team page generated");
-    });
+  });
 }
 
+const writeToFile = data => {
+  fs.writeFile("index.html", data, (err) => {
+    err ? console.log(err) : console.log("Team page generated");
+  });
+};
 
-
-init()
+init();
